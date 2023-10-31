865f876ece62b6ce39e60e8017d57c94dec6c88b
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-10-31 10:49:59 -0300

    feat: contact model

--- lib/contact_app/domain/contact_model.dart
+++ lib/contact_app/domain/contact_model.dart
@@ -1,1 +1,34 @@
-class Contact {}
+import 'dart:convert';
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Contact {
+  final String? id;
+  final String name;
+  final String email;
+  Contact({
+    this.id,
+    required this.name,
+    required this.email,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'email': email,
+    };
+  }
+
+  factory Contact.fromMap(Map<String, dynamic> map) {
+    return Contact(
+      id: map['id'] != null ? map['id'] as String : null,
+      name: map['name'] as String,
+      email: map['email'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Contact.fromJson(String source) =>
+      Contact.fromMap(json.decode(source) as Map<String, dynamic>);
+}
